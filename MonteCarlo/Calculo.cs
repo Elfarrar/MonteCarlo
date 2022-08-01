68d327b833d4591f1c4e6510877de604a07bdf0b
Rafael Veneroso Morici <rvenerosomorici@gmail.com>
2022-08-01 15:34:16 -0300

    Adicionar arquivos de projeto.

---
+++ MonteCarlo/Calculo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonteCarlo
+{
+    public class Calculo
+    {
+        public IEnumerable<Pontos> GerarPontos(int quantidade)
+        {
+            Pontos[] pontos = new Pontos[quantidade];
+            Random random = new Random();
+            for (int i = 0; i < quantidade; i++)
+            {
+                pontos[i] = new Pontos()
+                {
+                    X = random.NextDouble(),
+                    Y = random.NextDouble(),
+                };
+            }
+
+            return pontos;
+
+        }
+
+
+        public double Aproximacao(IEnumerable<Pontos> pontos)
+        {
+            int insideCount = 0;
+            int outsideCount = 0;
+
+            foreach (var p in pontos)
+            {
+                var resultado = (p.Y * p.Y) + (p.X * p.X);
+                if (resultado <= 1)
+                    insideCount++;
+                else
+                    outsideCount++;
+            }
+
+            return 4 * (double)insideCount / (double)pontos.Count();
+        }
+    }
+}
---
+++ MonteCarlo/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MonteCarlo;
+
+Calculo c = new Calculo();
+
+Console.WriteLine("Gerando Números!");
+
+var pontos = c.GerarPontos(100000);
+
+Console.WriteLine("Números Gerados!");
+
+var pi = c.Aproximacao(pontos);
+
+Console.WriteLine($"A Aproximação é {pi}");
